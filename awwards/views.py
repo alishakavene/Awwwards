e32e3bec4198d120b4a624bd429e05180aaabda5
alishakavene <akmunywoki@gmail.com>
2020-08-20 06:04:04 +0300

    create register form

--- awwards/views.py
+++ awwards/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def welcome(request):
     return render(request, 'all-awwards/base.html')
+
+def register(request):
+    form = UserCreationForm()
+    return render(request,'all-awwards/register.html',{'form':form})
